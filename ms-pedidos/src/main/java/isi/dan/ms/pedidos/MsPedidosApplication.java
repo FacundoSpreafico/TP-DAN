de78ebe236157befbd7bd05b6550d7b2c5c5e5ef
Facundo Spreafico <facuspreafico@gmail.com>
2024-07-05 16:34:34 -0300

    eureka configurado

--- ms-pedidos/src/main/java/isi/dan/ms/pedidos/MsPedidosApplication.java
+++ ms-pedidos/src/main/java/isi/dan/ms/pedidos/MsPedidosApplication.java
@@ -2,8 +2,10 @@ package isi.dan.ms.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class MsPedidosApplication {
 
 	public static void main(String[] args) {
