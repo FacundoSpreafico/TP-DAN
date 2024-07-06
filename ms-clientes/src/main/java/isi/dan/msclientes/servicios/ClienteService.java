8fbd8bd498167d0f56f7fc30c4a90acff73ea4f5
catriel <poncedeleoncatriel@gmail.com>
2024-07-06 10:58:43 -0300

    validacion de los saves en ClienteService

--- ms-clientes/src/main/java/isi/dan/msclientes/servicios/ClienteService.java
+++ ms-clientes/src/main/java/isi/dan/msclientes/servicios/ClienteService.java
@@ -1,11 +1,12 @@
 package isi.dan.msclientes.servicios;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import isi.dan.msclientes.dao.ClienteRepository;
 import isi.dan.msclientes.model.Cliente;
-
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +15,10 @@ public class ClienteService {
     
     @Autowired
     private ClienteRepository clienteRepository;
+    private BigDecimal numeroceroBD = new BigDecimal(0) ;
+
+    @Value("${cliente.maximo_descubierto.default}")
+    private BigDecimal defaultmd;
 
     public List<Cliente> findAll() {
         return clienteRepository.findAll();
@@ -24,7 +29,19 @@ public class ClienteService {
     }
 
     public Cliente save(Cliente cliente) {
-        return clienteRepository.save(cliente);
+  
+        if(cliente.getMaximoDescubierto().compareTo(numeroceroBD)==-1){
+
+        //aca falta poner el retutn bien con el error que tira
+            return cliente;
+        }
+        //si es null le pone el default y si es mayor a 0 lo guarda normalmente
+        else {
+            if(cliente.getMaximoDescubierto()==null){
+                cliente.setMaximoDescubierto(defaultmd);
+            }
+            return clienteRepository.save(cliente);
+        }
     }
 
     public Cliente update(Cliente cliente) {
